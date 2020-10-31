b1347755ce76a57f32e445ff1d52ba38824fddef
Peter Toth <tothpeter08@gmail.com>
2020-10-31 11:55:50 +0100

    Extract functionality to methods

--- fetch_words/lib/main.rb
+++ fetch_words/lib/main.rb
@@ -4,19 +4,14 @@ class Main
   PRONOUNS_EN = ['I', 'you', 'he', 'we', 'you all', 'they']
   PRONOUNS_ES = ['yo', 'tú', 'él', 'nosotros', 'vosotros', 'ellos']
 
+  attr_reader :infinitive_verb_es
+
   def self.start(infinitive_verb_es)
     new(infinitive_verb_es)
   end
 
   def initialize(infinitive_verb_es)
-    infinitive_verb_es = read_verb_es(infinitive_verb_es)
-
-    base_url = 'https://www.spanishdict.com/conjugate'
-
-    final_url = "#{base_url}/#{infinitive_verb_es}"
-    page_content = Net::HTTP.get(URI.parse(final_url))
-
-    all_data = JSON.parse page_content.match(/window\.SD_COMPONENT_DATA = (.+)/).to_a.last[0..-2]
+    set_infinitive_verb_es(infinitive_verb_es)
 
     infinitive_base_verb_en = all_data['verbTranslations']['pastParticiple']
 
@@ -35,14 +30,27 @@ class Main
 
   private
 
-  def read_verb_es(infinitive_verb_es)
+  def set_infinitive_verb_es(infinitive_verb_es)
     if infinitive_verb_es.nil?
       printf 'Infinitive verb in Español: '
-      gets.chomp
+      @infinitive_verb_es = gets.chomp
     else
       puts 'Infinitive verb in Español: ' + infinitive_verb_es
-      infinitive_verb_es
+      @infinitive_verb_es = infinitive_verb_es
     end
+  end
+
+  def all_data
+    @all_data ||= fetch_all_data
+  end
+
+  def fetch_all_data
+    base_url = 'https://www.spanishdict.com/conjugate'
+
+    final_url = "#{base_url}/#{infinitive_verb_es}"
+    page_content = Net::HTTP.get(URI.parse(final_url))
+
+    JSON.parse page_content.match(/window\.SD_COMPONENT_DATA = (.+)/).to_a.last[0..-2]
   end
 
   def read_from_console(label, default = '')
